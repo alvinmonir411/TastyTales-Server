b40077ed2503bc5d3b0c11c877c7eaa2952674f4
alvinmonir411 <moniruzzaman710@outlook.com>
2025-06-17 14:49:16 +0600

    delet nodemodules

--- index.jsx
+++ index.jsx
@@ -2,6 +2,8 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const port = 3000;
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
